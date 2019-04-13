ba46d173e61aa87c384b3eab24f44a86a16ea453
cassianasl <cassianasilveira64@gmail.com>
2019-04-13 16:09:54 -0300

    Add files via upload

--- frontend/src/Auth.js
+++ frontend/src/Auth.js
@@ -38,26 +38,37 @@ class Auth {
         return new Promise((resolve, reject) => {
             this.auth0.parseHash((err, authResult) => {
                 if (err) return reject(err);
-                
                 if (!authResult || !authResult.idToken) {
                     return reject(err);
                 }
-                
-                this.idToken = authResult.idToken;
-                this.profile = authResult.idTokenPayload;
-                // set the time that the id token will expire at
-                this.expiresAt = authResult.idTokenPayload.exp * 1000;
-
+                this.setSession(authResult);
                 resolve();
             });
         })
     }
-
+    
+    setSession(authResult) {
+        this.idToken = authResult.idToken;
+        this.profile = authResult.idTokenPayload;
+        // set the time that the id token will expire at
+        this.expiresAt = authResult.idTokenPayload.exp * 1000;
+    }
+    
     signOut() {
-        // clear id token, profile, and expiration
-        this.idToken = null;
-        this.profile = null;
-        this.expiresAt = null;
+        this.auth0.logout({
+            returnTo: 'http://localhost:3000',
+            clientID: 'ajlOn2zh6i49LJbXks7jqDw3Cm7TtYkf',
+        });
+    }
+
+    silentAuth() {
+        return new Promise((resolve, reject) => {
+            this.auth0.checkSession({}, (err, authResult) => {
+                if (err) return reject(err);
+                this.setSession(authResult);
+                resolve();
+            });
+        });
     }
 }
 

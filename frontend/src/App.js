c7831c61c94c3de431054db2a2440779a65cc02e
cassianasl <cassianasilveira64@gmail.com>
2019-04-13 14:56:17 -0300

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import NavBar from './NavBar/NavBar.js';
+import Questions from './Questions/Questions';
+import Question from './Question/Question';
+import {Route} from 'react-router-dom';
+import Callback from './Callback';
+import NewQuestion from './NewQuestion/NewQuestion';
+import SecuredRoute from './SecuredRoute/SecuredRoute';
 
 class App extends Component {
   render() {
     return (
       <div>
         <NavBar/>
-        <p>Work in progress...</p>
+        
+        <Route exact path='/' component={Questions}/>
+        
+        <Route exact path='/question/:questionId' component={Question}/>
+        
+        <Route exact path='/callback' component={Callback}/>
+        
+        <SecuredRoute path='/new-question' component={NewQuestion} />
       </div>
     );
   }
--- frontend/src/Questions/Questions.js
+++ frontend/src/Questions/Questions.js
@@ -13,29 +13,41 @@ class Questions extends Component {
 
     async componentDidMount() {
         const questions = (await axios.get('http://localhost:8081/')).data;
-    
-        this.setState({
-            questions,
-        });
+
+        console.log(questions)
+        this.setState({questions});
     }
 
     render() {
         return (
             <div className="container">
                 <div className="row">
+                    <Link to="/new-question">
+                        <div className="card text-white bg-secondary mb-3">
+                            <div className="card-header">Need help? Ask here!</div>
+                            <div className="card-body">
+                                <h4 className="card-title">+ New Question</h4>
+                                <p className="card-text">Don't worry. Help is on the way!</p>
+                            </div>
+                        </div>
+                    </Link>
+                    {/* <!-- ... loading questions message ... -->
+                        <!-- ... questions' cards ... --> */}
                     {this.state.questions === null && <p>Loading questions...</p>}
-                    {this.state.questions && this.state.questions.map(question => (
-                        <div key={question.id} className="col-sm-12 col-md-4 col-lg-3">
-                            <Link to={`/question/${question.id}`}>
-                                <div className="card text-white bg-success mb-3">
-                                    <div className="card-header">Answers: {question.answers}</div>
-                                    <div className="card-body">
-                                        <h4 className="card-title">{question.title}</h4>
-                                        <p className="card-text">{question.description}</p>
+                    {
+                        this.state.questions && this.state.questions.map(question => (
+                            <div key={question.id} className="col-sm-12 col-md-4 col-lg-3">
+                                <Link to={`/question/${question.id}`}>
+                                    <div className="card text-white bg-success mb-3">
+                                        <div className="card-header">Answers: {question.answers}</div>
+                                        <div className="card-body">
+                                            <h4 className="card-title">{question.title}</h4>
+                                            <p className="card-text">{question.description}</p>
+                                        </div>
                                     </div>
-                                </div>
-                            </Link>
-                        </div>))
+                                </Link>
+                            </div>
+                        ))
                     }
                 </div>
             </div>
